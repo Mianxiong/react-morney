d8983180ada6ca0a95b90d7a85dde916900a1f8a
Mianxiong <757134184@qq.com>
2023-02-12 20:26:18 +0800

    完成标签的选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useState} from "react";
 
-const _TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #ffffff;
   padding: 12px 16px;
   flex-grow: 1;
@@ -9,18 +9,25 @@ const _TagsSection = styled.section`
   flex-direction: column;
   justify-content: flex-end;
   align-items: flex-start;
-  > ol{
+
+  > ol {
     margin: 0 -12px;
-    > li{
+
+    > li {
       background: #D9d9d9;
       border-radius: 18px;
       display: inline-block;
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+
+      &.selected {
+        background: #f60;
+      }
     }
   }
-  > button{
+
+  > button {
     background: none;
     border: none;
     padding: 0 4px;
@@ -30,17 +37,34 @@ const _TagsSection = styled.section`
   }
 `
 
-const TagsSection: React.FC = () => {
-    return(
-        <_TagsSection>
+const TagsSection: React.FC = (props) => {
+    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行'])
+    const [selectedTags, setSelectedTags] = useState<string[]>([])
+    const onAddTag = () => {
+        const tagName = window.prompt('新标签的名称为')
+        if (tagName !== null) {
+            setTags([...tags, tagName])
+        }
+    }
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag)
+        if (index >= 0) {
+            // 如果 tag 已被选中，就复制所有没有被选中的 tag，作为新的 selectedTag
+            setSelectedTags(selectedTags.filter(t => t !== tag))
+        } else {
+            setSelectedTags([...selectedTags, tag])
+        }
+    }
+    const getClass = (tag: string) => selectedTags.indexOf(tag) >= 0 ? 'selected' : ''
+    return (
+        <Wrapper>
             <ol>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
+                {tags.map(tag =>
+                    <li key={tag} onClick={() => onToggleTag(tag)} className={getClass(tag)}>
+                        {tag}</li>)}
             </ol>
-            <button>新增标签</button>
-        </_TagsSection>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
     )
 }
 export {TagsSection}
