4fa611fcc94441ffa4f548a2e7af84cfcbbb3721
Mianxiong <757134184@qq.com>
2023-02-13 18:39:38 +0800

    创建单独的Button和Input组件

---
+++ src/components/Input.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+    display: flex;
+    align-items: center;
+
+    > span {
+      margin-right: 16px;
+      white-space: nowrap
+    }
+
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+`
+
+type Props = {
+  label: string
+} & React.InputHTMLAttributes<HTMLInputElement>
+
+const Input: React.FC<Props> = (props)=>{
+  const {label, children,...rest} = props;
+  return (
+    <Label>
+      <span>{props.label}</span>
+      {/*受控组件*/}
+      {/*<input type="text" placeholder="在这里添加备注"*/}
+      {/*       value={note} onChange={(e) => setNote(e.target.value)}/>*/}
+      {/*非受控组件*/}
+      {/*<input type={props.type} placeholder={props.placeholder}*/}
+      {/*       defaultValue={props.defaultValue}*/}
+      {/*        onBlur={props.onBlur}/>*/}
+      <input {...rest}/>
+    </Label>
+  )
+}
+
+export {Input}
+
--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,28 +1,11 @@
 import styled from 'styled-components';
-import React, {useRef} from 'react';
+import React, {ChangeEventHandler, useRef} from 'react';
+import {Input} from '../../components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
   padding: 0px 16px;
   font-size: 14px;
-
-  > label {
-    display: flex;
-    align-items: center;
-
-    > span {
-      margin-right: 16px;
-      white-space: nowrap
-    }
-
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `;
 
 type Props = {
@@ -32,24 +15,20 @@ type Props = {
 
 const NoteSection: React.FC<Props> = (props) => {
   const note = props.value
-  const refInput = useRef<HTMLInputElement>(null);
   //非受控组件
-  const onBlur = () => {
-    if (refInput.current !== null) {
-      props.onChange(refInput.current.value);
-    }
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+      props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        {/*受控组件*/}
+      <Input label="备注" type="text" value={note}
+             onChange={onChange}>
+        {/*<span>备注</span>*/}
         {/*<input type="text" placeholder="在这里添加备注"*/}
-        {/*       value={note} onChange={(e) => setNote(e.target.value)}/>*/}
-        {/*非受控组件*/}
-        <input type="text" placeholder="在这里添加备注"
-               ref={refInput} defaultValue={note} onBlur={onBlur}/>
-      </label>
+        {/*ref={refInput}*/}
+        {/*defaultValue={note}*/}
+        {/*onBlur={onBlur}/>*/}
+      </Input>
     </Wrapper>
   );
 };
