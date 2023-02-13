1c8ad32ecb771f174270abfab0b8778af4c12553
Mianxiong <757134184@qq.com>
2023-02-13 20:24:40 +0800

    编辑 tag 名

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -9,10 +9,29 @@ const defaultTags = [
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag=> tag.id === id)[0]
+  const findTagIndex = (id: number) => {
+    let result = -1
+    for (let i = 0; i<tags.length;i++) {
+      if(tags[i].id === id) {
+        result = i
+        break;
+      }
+    }
+    return result;
+  }
+  const updateTag = (id:number, obj: {name: string}) => {
+    const index = findTagIndex(id);
+    // 深拷贝 tags 得到 tagsClone
+    const tagsClone = JSON.parse(JSON.stringify(tags))
+    tagsClone.splice(index,1,{id:id,name:obj.name})
+    setTags(tagsClone)
+  }
   return {
     tags: tags,
     setTags: setTags,
-    findTag: findTag
+    findTag: findTag,
+    updateTag: updateTag,
+    findTagIndex: findTagIndex
   };
 };
 export {useTags};
--- src/views/Money/Tag.tsx
+++ src/views/Money/Tag.tsx
@@ -21,16 +21,16 @@ const Topbar = styled.header`
   line-height: 20px;
   padding: 14px;
   background: white;
-`
+`;
 const InputWrapper = styled.div`
   background: white;
   padding: 0 16px;
   margin-top: 8px;
-`
-const Tag: React.FC = (props)=>{
-  const {findTag} = useTags()
-  let { id } = useParams<Params>()
-  const tag = findTag(parseInt(id))
+`;
+const Tag: React.FC = (props) => {
+  const {findTag, updateTag} = useTags();
+  let {id} = useParams<Params>();
+  const tag = findTag(parseInt(id));
   return (
     <Layout>
       <Topbar>
@@ -39,7 +39,10 @@ const Tag: React.FC = (props)=>{
         <Icon/>
       </Topbar>
       <InputWrapper>
-        <Input label="标签名" type="text" placeholder="标签名" value={tag.name}/>
+        <Input label="标签名" type="text" placeholder="标签名"
+               value={tag.name} onChange={(e) => {
+         updateTag(tag.id,{name:e.target.value})
+        }}/>
       </InputWrapper>
       <Center>
         <Space/>
@@ -47,7 +50,7 @@ const Tag: React.FC = (props)=>{
         <Button>删除标签</Button>
       </Center>
     </Layout>
-  )
-}
+  );
+};
 
-export {Tag}
+export {Tag};
